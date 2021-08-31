f61851c1ebe23d6e91742e5094eb02798ce902af
Andre <Andre@ANDREPC>
2021-08-31 10:42:59 -0300

    Add project files.

---
+++ FilmesApi/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Data.Dtos
+{
+    public class CreateFilmeDto
+    {
+        
+        [Required(ErrorMessage = "O Título é de preenchimento obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Diretor é de preenchimento obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = "O Genero deve possuir apenas 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "O Filme deve possuir no mínimo 1 e no máximo 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
---
+++ FilmesApi/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Models
+{
+    public class Filme
+    {        
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="O Título é de preenchimento obrigatorio" )]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Diretor é de preenchimento obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength( 30, ErrorMessage = "O Genero deve possuir apenas 30 caracteres")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage ="O Filme deve possuir no mínimo 1 e no máximo 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
